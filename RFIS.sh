2959cc64bdb6f5990d2ab8a4673de4e438631019
sgeu the coding cat <139828915+sgeuthecodingcat@users.noreply.github.com>
2024-07-29 07:33:15 -0400

    Update RFIS.sh

--- RFIS.sh
+++ RFIS.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # Added shebang
+now=`date +"%F%::z"`
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@" 
 # checks for root priveleges (UID 0) and if it is not being ran with sudo will re-exec the script as sudo using "sudo bash"
 echo ==============================
 echo RPM FUSION INSTALLATION SCRIPT
 echo FOR USE WITH BASH ONLY
+echo TESTING BRANCH
 echo ==============================
 echo Written by Sgeuthecodingcat
-echo this CAN break systems because I am just bad at coding
+echo THIS IS THE TESTING BRANCH - INSTABLILITY IS LIKELY
 echo Use at your own risk!
 echo Press CTRL+C to quit, Script will continue in 5 seconds.
 sleep 5
 # stops script for 5 seconds, simple as that
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf config-manager --enable fedora-cisco-openh264 -y
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y | tee -a rfis_log-$date
+sudo dnf config-manager --enable fedora-cisco-openh264 -y 
 # self explanitory - enables fedora cisco openh264 repos & install rpm fusion repo
 clear
 echo ================
@@ -26,7 +28,7 @@ clear
 echo ===================
 echo MULTIMEDIA INSTALL
 echo ===================
-sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y && sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y && sudo dnf update @sound-and-video -y
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y && sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y && sudo dnf update @sound-and-video -y | tee -a rfis_log-$date
 # simply the multimedia step on their website
 clear
 echo ===================
