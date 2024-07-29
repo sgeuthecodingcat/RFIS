d944f69b50f33d6d57adc1da165dfb0d604bab9a
sgeu the coding cat <139828915+sgeuthecodingcat@users.noreply.github.com>
2024-07-29 06:52:44 -0400

    Add files via upload

--- RFIS.sh
+++ RFIS.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Added shebang
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@" 
+# checks for root priveleges (UID 0) and if it is not being ran with sudo will re-exec the script as sudo using "sudo bash"
 echo ==============================
 echo RPM FUSION INSTALLATION SCRIPT
 echo FOR USE WITH BASH ONLY
@@ -7,8 +11,10 @@ echo this CAN break systems because I am just bad at coding
 echo Use at your own risk!
 echo Press CTRL+C to quit, Script will continue in 5 seconds.
 sleep 5
+# stops script for 5 seconds, simple as that
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf config-manager --enable fedora-cisco-openh264 -y
+# self explanitory - enables fedora cisco openh264 repos & install rpm fusion repo
 clear
 echo ================
 echo INSTALL COMPLETE
@@ -21,6 +27,7 @@ echo ===================
 echo MULTIMEDIA INSTALL
 echo ===================
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y && sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y && sudo dnf update @sound-and-video -y
+# simply the multimedia step on their website
 clear
 echo ===================
 echo MULTIMEDIA COMPLETE
